9f9e3b3a369d09f22a7e5ea2f303befd7b6b78ba
HattoriHamzo <77831591+HattoriHamzo@users.noreply.github.com>
2022-05-25 23:49:49 +0200

    Delete src/main directory

--- src/main/model/dataBase.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-engine = sqlalchemy.create_engine("superSecret")
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
--- src/main/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-cd /home/ubuntu/gpuProject/src/main && python3 -m luigi --module main ExecuteStreamLit --local-scheduler
